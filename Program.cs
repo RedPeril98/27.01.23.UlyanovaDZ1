0e1a1f27d8f6305005a82741073eff62cd972c3b
Дарья <red_peril@bk.ru>
2023-01-27 09:56:30 +0300

    Задача 8

--- Program.cs
+++ Program.cs
@@ -57,6 +57,7 @@ Console.WriteLine("max = " + max);
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+/*
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
@@ -70,4 +71,23 @@ else
 {
 Console.WriteLine("Нет");
 }
+*/
 
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+while(evenNumber <= number)
+{
+Console.Write(evenNumber + " ");
+evenNumber = evenNumber + 2;
+}
+}
+*/
